8ca5f0bcdfdec1fd942d37aa8fd14eac19b94680
lsarteaga <lsarteaga_dev@protonmail.ch>
2020-04-29 22:14:16 -0500

    se cambio el mensaje flash

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -24,7 +24,8 @@ def register():
             'SELECT id_user FROM user WHERE username = ?',(form.username.data,)
         ).fetchone()
         if value is not None:
-            error = 'User {} is already registered'.format(form.username.data)
+            error = 'Username {} is already in use, try another'.format(form.username.data)
+            flash(error)
         else:
             db.execute(
                 'INSERT INTO user (username, password) VALUES (?,?)',
@@ -32,12 +33,23 @@ def register():
             )
             db.commit()
             return redirect('/')
-        flash(error)
 
     return render_template('auth/register.html', form=form)
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6,message='Password too short')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6,message='Password too short')], description = 'Password must at least 6 characters')
     confirm_pwd = PasswordField('Repeat Password',
         validators=[DataRequired(), EqualTo('password',message='Passwords must match')])
+
+@bp.route('/login',methods=['GET','POST'])
+def login():
+
+
+
+
+    return render_template('auth/login.html')
+
+class LogginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6,message='Password too short')], description = 'Password must at least 6 characters')
